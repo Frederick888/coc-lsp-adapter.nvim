f1e980ae1d592d9221ef86a901e3d0e3218f185a
Frederick Zhang <frederick888@tsundere.moe>
2023-03-21 14:06:09 +1100

    feat: Initial working copy

---
+++ README.md
@@ -0,0 +1,69 @@
+# Project Status: Experimental
+
+This project was created only to allow [nvim-jdtls][1] to leverage
+[coc.nvim][2] and has only been tested in this scenario.
+
+
+# Description
+
+'Polyfills' [`vim.lsp.client`][3] calls with coc.nvim LSP client. For
+example, it intercepts `vim.lsp.get_active_clients()` and returns a
+table of (somewhat) native-client-compatible clients if the native API
+returned no clients.
+
+These polyfilled clients then redirect calls like `request()`,
+`request_async()`, etc., to their coc.nvim counterparts.
+
+
+# Configuration
+
+```vim
+Plug 'Frederick888/coc-lsp-adapter.nvim'
+Plug 'mfussenegger/nvim-dap'
+let g:nvim_jdtls = 1
+Plug 'mfussenegger/nvim-jdtls'
+```
+
+```lua
+M = {}
+
+M.coc_dap_initialised = false
+M.coc_dap_timer = nil
+M.coc_dap_timer = vim.fn.timer_start(1000, function()
+  if M.coc_dap_initialised or vim.g.coc_service_initialized ~= 1 then
+    return
+  end
+
+  require('coc-lsp-adapter')
+  local clients = vim.lsp.get_active_clients()
+  if vim.tbl_count(clients) == 0 then
+    return
+  end
+
+  for _, client in pairs(clients) do
+    if client.name == 'java' then
+      require('jdtls').setup_dap({ hotcodereplace = 'auto' })
+      require('jdtls.dap').setup_dap_main_class_configs()
+      vim.notify('Done setting up nvim-dap and coc-java')
+    end
+  end
+
+  M.coc_dap_initialised = true
+  vim.fn.timer_stop(M.coc_dap_timer)
+end, {
+  ['repeat'] = -1,
+})
+
+return M
+```
+
+
+# Contributions
+
+This is an experimental project and I currently do not plan to implement
+any new features. Contributions are welcome if they are maintainable.
+
+
+[1]: https://github.com/mfussenegger/nvim-jdtls
+[2]: https://github.com/neoclide/coc.nvim
+[3]: https://neovim.io/doc/user/lsp.html#vim.lsp.client
---
+++ lua/coc-lsp-adapter.lua
@@ -0,0 +1,92 @@
+local M = {}
+
+local native = {
+    get_active_clients = vim.lsp.get_active_clients,
+    buf_get_clients = vim.lsp.buf_get_clients,
+}
+
+local server_capabilities = {
+    java = {
+        executeCommandProvider = {
+            commands = {
+                'vscode.java.test.findTestTypesAndMethods',
+                'vscode.java.checkProjectSettings',
+                'vscode.java.resolveMainClass',
+                'vscode.java.resolveClasspath',
+                'vscode.java.startDebugSession',
+                'vscode.java.test.junit.argument',
+                'java.project.getClasspaths',
+            }
+        }
+    }
+}
+
+local function get_active_clients(filter)
+    filter = filter or {}
+    local native_clients = native.get_active_clients(filter)
+    if vim.tbl_count(native_clients) > 0 or vim.g.coc_enabled ~= 1 then
+        return native_clients
+    end
+    local coc_clients = {}
+    local coc_services = vim.fn['CocAction']('services')
+    for id, service in pairs(coc_services) do
+        local client = {
+            id = id,
+            name = service.id,
+            language_ids = service.languageIds,
+            server_capabilities = server_capabilities[service.id] or {},
+        }
+
+        ---@diagnostic disable-next-line: unused-local
+        function client.request(method, params, callback, bufnr)
+            local callback_wrapper = function(err, result)
+                if err == vim.NIL then
+                    err = nil
+                end
+                callback(err, result)
+            end
+            vim.fn['CocRequestAsync'](client.name, method, params, callback_wrapper)
+        end
+
+        ---@diagnostic disable-next-line: unused-local
+        function client.request_sync(method, params, timeout_ms, bufnr)
+            vim.fn['CocRequest'](client.name, method, params)
+        end
+
+        function client.notify(method, params)
+            vim.fn['CocNotify'](client.name, method, params)
+        end
+
+        table.insert(coc_clients, client)
+    end
+    if type(filter.id) == 'number' then
+        return { coc_clients[filter.id] }
+    end
+    if type(filter.name) == 'string' then
+        for _, client in pairs(coc_clients) do
+            if client.name == filter.name then
+                return { client }
+            end
+        end
+        return {}
+    end
+    return coc_clients
+end
+
+local function buf_get_clients(bufnr)
+    return get_active_clients({
+        buffer = bufnr,
+    })
+end
+
+vim.lsp.get_active_clients = get_active_clients
+vim.lsp.buf_get_clients = buf_get_clients
+
+M.native = native
+M.server_capabilities = server_capabilities
+M.lsp = {
+    get_active_clients = get_active_clients,
+    buf_get_clients = buf_get_clients,
+}
+
+return M
